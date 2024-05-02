0cf8a0c9bd6acd7f718da6aa729b127663de8c21
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-03 01:12:33 +0700

    feat: search location icon

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,10 +1,19 @@
+"use client"
 import React from 'react'
+import uniskyLogo from "../../public/unisky-logo.svg"
+import searchIcon from "../../public/search.svg"
+import Image from 'next/image'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='flex items-center bg-[#81ACDA] w-full h-[72px]'>
+        <div className='flex flex-row items-center justify-between w-full px-5'>
+          <Image className='w-[82px] h-auto' src={uniskyLogo} alt='unisky logo'/> 
+          <Image className='w-[28px] h-auto' src={searchIcon} alt='search icon'/>
+        </div>
+      </div>
+    </>
   )
 }
 
