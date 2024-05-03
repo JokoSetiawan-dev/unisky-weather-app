86aa3db34989c9746440aafd95635ff5664467a3
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-04 00:23:39 +0700

    style: remove absolute position

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -7,11 +7,13 @@ import Image from 'next/image'
 function Navbar() {
   return (
     <>
-      <div className='flex items-center bg-[#81ACDA] w-full h-[72px]'>
+      <div className='z-10 w-full top-0'>
+      <div className='flex items-center top-0 bg-[#81ACDA] w-full h-[72px]'>
         <div className='flex flex-row items-center justify-between w-full px-5'>
           <Image className='w-[82px] h-auto' src={uniskyLogo} alt='unisky logo'/> 
           <Image className='w-[28px] h-auto' src={searchIcon} alt='search icon'/>
         </div>
+      </div>
       </div>
     </>
   )
