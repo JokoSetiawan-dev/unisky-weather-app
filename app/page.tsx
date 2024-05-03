dd63521a93bad62ced1375e9ab2945983f8743e9
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-04 00:24:29 +0700

    feat: daily and weekly forecast components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import CurrentWeather from "./Components/CurrentWeather";
+import DailyForecast from "./Components/DailyForecast";
 import Navbar from "./Components/Navbar";
+import WeeklyForecast from "./Components/WeeklyForecast";
 
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <main>
       <Navbar/>
       <CurrentWeather/>
+      <DailyForecast/>
+      <WeeklyForecast/>
     </main>
   );
 }
