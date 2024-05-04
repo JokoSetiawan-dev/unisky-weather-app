962f84667e936dbc46d07c349a19f996c2749a15
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-05 00:28:38 +0700

    feat: add new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import CurrentWeather from "./Components/CurrentWeather";
 import DailyForecast from "./Components/DailyForecast";
+import HumidityIndex from "./Components/HumidityIndex";
 import Navbar from "./Components/Navbar";
+import Pressure from "./Components/Pressure";
+import SunriseSet from "./Components/SunriseSet";
+import UvIndex from "./Components/UvIndex";
 import WeeklyForecast from "./Components/WeeklyForecast";
 
 
@@ -11,6 +15,14 @@ export default function Home() {
       <CurrentWeather/>
       <DailyForecast/>
       <WeeklyForecast/>
+      <div className="flex w-full items-center justify-between p-4">
+        <UvIndex/>
+        <HumidityIndex/>
+      </div>
+      <div>
+        <Pressure/>
+        <SunriseSet/>
+      </div>
     </main>
   );
 }
