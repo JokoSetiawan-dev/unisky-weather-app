2c1576b33ddb6c9583f9ba458d8f5d9758c6ed8d
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-07 20:46:53 +0700

    styled: change layouting sunrise and sunset components

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <UvIndex/>
         <HumidityIndex/>
       </div>
-      <div>
+      <div className="flex gap-5 items-center justify-center">
         <Pressure/>
         <SunriseSet/>
       </div>
