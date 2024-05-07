14d1e9c453474dee4ada5c2405731f15b0aec1b6
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-07 23:22:04 +0700

    feat: create footer

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='pb-4 mt-10'>
+      <div className='flex flex-col items-center justify-center gap-1'>
+        <h1 className='text-xs font-extrabold'>Unisky Weather App V1.0.0</h1>
+        <h1 className='text-[8px]'>Copyright © 2024 by SEIER CORP.</h1>
+      </div>
+    </div>
+  )
+}
