5b06e5b28aff158e039bfa06265014cbc6322205
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-07 23:21:20 +0700

    style: change layout of the component

--- app/Components/SunriseSet.tsx
+++ app/Components/SunriseSet.tsx
@@ -8,12 +8,12 @@ export default function SunriseSet() {
     <div className='flex w-[185.2px] h-[110px] justify-around bg-white rounded-3xl shadow-lg'>
       <div className='flex flex-col items-center justify-center'>
         <Image src={sunriseIcon} alt='sunrise icon'/>
-        <h1 className='font-bold mt-2'>Sunrise</h1>
+        <h1 className='font-bold text-sm mt-2'>Sunrise</h1>
         <p className='text-sm'>06:00</p>
       </div>
       <div className='flex flex-col items-center justify-center'>
         <Image src={sunsetIcon} alt='sunrise icon'/>
-        <h1 className='font-bold mt-2'>Sunset</h1>
+        <h1 className='font-bold text-sm mt-2'>Sunset</h1>
         <p className='text-sm'>18:00</p>
       </div>
     </div>
