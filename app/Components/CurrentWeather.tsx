960e5814c1b4c0ae5ab99f1324a5d2d083cb62cd
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-15 23:51:00 +0700

    feat: update information from current data

--- app/Components/CurrentWeather.tsx
+++ app/Components/CurrentWeather.tsx
@@ -2,78 +2,125 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { useGlobalContext } from '../context/globalContext'
-import { kelvinToCelsius } from '../utils/misc'
-import { broken, dayClear, drizzleCond, fewCloudDay, fewCloudNight, nightClear, scattered } from '../utils/weatherIcon'
+import { kelvinToCelsius, toProperCase } from '../utils/misc'
+import {
+  broken,
+  dayClear,
+  dayRain,
+  drizzleCond,
+  fewCloudDay,
+  fewCloudNight,
+  nightClear,
+  nightRain,
+  scattered,
+  thunder,
+} from '../utils/weatherIcon'
 
 export default function CurrentWeather() {
+  const { forecast } = useGlobalContext()
 
-  const {forecast} = useGlobalContext()
-
-  const {main, timezone, name, weather} = forecast
-
-  if (!forecast || !weather) {
+  if (!forecast || !forecast.weather) {
     return <div>Loading...</div>
   }
+
+  const { main, name, weather } = forecast
+  const { main: weatherMain, description } = weather[0]
 
   const temp = kelvinToCelsius(main?.temp)
   const minTemp = kelvinToCelsius(main?.temp_min)
   const maxTemp = kelvinToCelsius(main?.temp_max)
 
   const [localTime, setLocalTime] = useState(true)
-  const [currentDate, setCurrentDate] = useState<string>("")
+  const [dayName, setDayName] = useState("")
+  const [day, setDay] = useState(0)
+  const [month, setMonth] = useState("")
+  const [year, setYear] = useState(0)
 
   useEffect(() => {
     const determineDayOrNight = () => {
-      const now = new Date();
-      const hours = now.getHours();
-      const dayTime = hours >= 6 && hours < 18; 
-      setLocalTime(dayTime);
-    };
+      const date = new Date()
+      const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+      const dayName = days[date.getDay()]
 
-    determineDayOrNight();
+      const day = date.getDate()
 
-    const intervalId = setInterval(determineDayOrNight, 60000);
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ]
+      const monthName = months[date.getMonth()]
 
-    return () => clearInterval(intervalId);
-  }, []);
+      const year = date.getFullYear()
+      const hours = date.getHours()
+      const dayTime = hours >= 6 && hours < 18
 
-  const {main: weatherMain, description} = weather[0]
+      setLocalTime(dayTime)
+      setDayName(dayName)
+      setDay(day)
+      setMonth(monthName)
+      setYear(year)
+    }
+
+    determineDayOrNight()
+
+    const intervalId = setInterval(determineDayOrNight, 60000)
+
+    return () => clearInterval(intervalId)
+  }, [])
 
   const getIcon = () => {
     switch (weatherMain) {
-        case "Drizzle":
-            return drizzleCond;
-        case "Clear":
-            return localTime ? dayClear : nightClear;
-        case "Clouds":
-            if (description === "few clouds") {
-                return localTime ? fewCloudDay : fewCloudNight;
-            } else if (description === "scattered clouds") {
-                return scattered;
-            } else if (description === "broken clouds" || description === "overcast clouds") {
-                return broken;
-            }
-        
+      case "Drizzle":
+        return drizzleCond
+      case "Clear":
+        return localTime ? dayClear : nightClear
+      case "Clouds":
+        if (description === "few clouds") {
+          return localTime ? fewCloudDay : fewCloudNight
+        } else if (description === "scattered clouds") {
+          return scattered
+        } else if (description === "broken clouds" || description === "overcast clouds") {
+          return broken
+        }
+        break
+      case "Rain":
+        return localTime ? dayRain : nightRain
+      case "Thunderstorm":
+        return thunder
+      default:
+        return drizzleCond
     }
-};
+  }
 
+  const formattedDescription = toProperCase(description)
+  const weatherIcon = getIcon()  // Call getIcon to get the actual icon
 
   return (
-    <div className='relative bg-[#81ACDA] h-[497.6px] w-full px-5'>
+    <div className='bg-[#81ACDA] h-[497.6px] w-full px-5'>
       <div className='flex flex-col h-full justify-between py-3'>
         <div className='pt-5'>
-            <p className='font-bold text-white text-base'>Pekanbaru</p>
-            <p className='text-[12px] text-white'>Friday, 3 May 2024</p>
+          <p className='font-bold text-white text-base'>{name}</p>
+          <p className='text-[12px] text-white'>{dayName}, {day} {month} {year}</p>
         </div>
-        <div>
-            {/* <Image className='absolute top-0 left-0 px-5 mt-16' src={drizzleCond} alt='Weather Condition Clear'/> */}
+        <div className='flex justify-center items-center'>
+          <Image className='items-center' src={weatherIcon ?? ""} alt={`Weather Condition ${weatherMain}`} />
         </div>
         <div className='flex justify-between items-center px-3'>
-            <h1 className='text-[80px] font-bold text-white'>24°</h1>
-            <div className='flex flex-col justify-end items-end text-white'>
-                <p className='font-bold text-[20px]'>Rainy</p>
-                <p className='text-[12px]'>Low: 20°  High: 30°</p>
-            </div>
+          <h1 className='text-[80px] font-bold text-white'>{temp}°</h1>
+          <div className='flex flex-col justify-end items-end text-white'>
+            <p className='font-bold text-[20px]'>{formattedDescription}</p>
+            <p className='text-[12px]'>Low: {minTemp}°  High: {maxTemp}°</p>
+          </div>
         </div>
       </div>
     </div>
