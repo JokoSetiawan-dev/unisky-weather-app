7deb4abc203ae7923a77deaa17c19814f85e0418
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-15 23:53:27 +0700

    feat: add weather icon

--- app/utils/weatherIcon.tsx
+++ app/utils/weatherIcon.tsx
@@ -1,23 +1,25 @@
-import clearDay from "@/public/clear-day.png"
-import clearNight from "@/public/clear-night.png"
+"use client"
+import clearDay from "@/public/clear-day.png";
+import clearNight from "@/public/clear-night.png";
 import fewCloudsDay from "@/public/few-clods-day.png"
-import fewCloudsNight from "@/public/few-clods-night.png"
-import scatteredCloud from "@/public/scattered-clouds.png"
-import brokenClouds from "@/public/broken-clouds.png"
-import showerRain from "@/public/shower-rain.png"
-import drizzle from "@/public/shower-rain.png"
-import rainNight from "@/public/rain-night.png"
-import rainDay from "@/public/rain-day.png"
-import thunderstorm from "@/public/thunderstorm.png"
+import fewCloudsNight from "@/public/few-clods-night.png";
+import scatteredCloud from "@/public/scattered-clouds.png";
+import brokenClouds from "@/public/broken-clouds.png";
+import showerRain from "@/public/shower-rain.png";
+import drizzle from "@/public/shower-rain.png";
+import rainNight from "@/public/rain-night.png";
+import rainDay from "@/public/rain-day.png";
+import thunderstorm from "@/public/thunderstorm.png";
 
-export const dayClear = {clearDay}
-export const nightClear = {clearNight}
-export const fewCloudDay = {fewCloudsDay}
-export const fewCloudNight = {fewCloudsNight}
-export const scattered = {scatteredCloud}
-export const broken = {brokenClouds}
-export const rainShower = {showerRain}
-export const drizzleCond = {drizzle}
-export const nightRain = {rainNight}
-export const dayRain = {rainDay}
-export const thunder = {thunderstorm}
+// Exporting images directly
+export const dayClear = clearDay;
+export const nightClear = clearNight;
+export const fewCloudDay = fewCloudsDay;
+export const fewCloudNight = fewCloudsNight;
+export const scattered = scatteredCloud;
+export const broken = brokenClouds;
+export const rainShower = showerRain;
+export const drizzleCond = drizzle;
+export const nightRain = rainNight;
+export const dayRain = rainDay;
+export const thunder = thunderstorm;
