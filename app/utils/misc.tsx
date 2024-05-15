e12df56d8370318a4fe86883486253565e90126a
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-15 23:53:09 +0700

    feat: create to proper case function for formatting

--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,3 +1,9 @@
 export const kelvinToCelsius = (kelvin:number) => {
     return Math.round(kelvin - 273.15)
 }
+
+export const toProperCase = (str: string) => {
+    return str.replace(/\w\S*/g, (word) =>
+        word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+    );
+};
