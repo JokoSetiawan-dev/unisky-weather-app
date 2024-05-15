8aaaaf1a71ade9633996e68eea8bc4b14f147172
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-05-15 23:52:33 +0700

    feat: search input for location

--- app/Components/Ui/Search.tsx
+++ app/Components/Ui/Search.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import locationIcon from "../../../public/location-icon.svg"
-import { Ghost } from 'lucide-react'
+} from "@/components/ui/dialog";
+import { Command, CommandInput } from "@/components/ui/command";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import locationIcon from "../../../public/location-icon.svg";
 
-export default function Search() {
+const Search: React.FC = () => {
   return (
     <div>
       <Dialog>
         <DialogTrigger>
-          <Button className='p-0' variant="ghost">
-           <Image className='w-[25px] h-auto' src={locationIcon} alt='search icon'/>
+          <Button className="p-0" variant="ghost">
+            <Image
+              className="w-[25px] h-auto"
+              src={locationIcon}
+              alt="search icon"
+            />
           </Button>
         </DialogTrigger>
-        <DialogContent className='m max-w-[330px] rounded-3xl'>
-          <DialogHeader className='text-left'>
-            <DialogTitle className='text-sm'>Choose Location</DialogTitle>
-            <DialogDescription className='text-[10px]'>Choose the Locations You Want to See</DialogDescription>
+        <DialogContent className="m max-w-[330px] rounded-3xl">
+          <DialogHeader className="text-left">
+            <DialogTitle className="text-sm">Choose Location</DialogTitle>
+            <DialogDescription className="text-[10px] pb-3">
+              Choose the Locations You Want to See
+            </DialogDescription>
+            <Command className="rounded-full shadow-lg">
+              <CommandInput placeholder="Search your location..." />
+            </Command>
           </DialogHeader>
         </DialogContent>
       </Dialog>
     </div>
-  )
-}
+  );
+};
+
+export default Search;
