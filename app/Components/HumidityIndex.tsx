3511393c0dcef631772639a7d9800e4fa3ca31a4
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-06-07 21:57:51 +0700

    feat: get data from api

--- app/Components/HumidityIndex.tsx
+++ app/Components/HumidityIndex.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import humidityImage from "../../public/humidity-inage.png"
 import Image from 'next/image'
+import { useGlobalContext } from '../context/globalContext'
 
 export default function HumidityIndex() {
+
+  const {forecast} = useGlobalContext()
+
+  const { main} = forecast
+
+  const humidityPersentage = main?.humidity
+
   return (
-    <div className="relative w-[170px] h-[170px]">
+    <div className="relative w-[190px] h-[190px]">
       <Image src={humidityImage} alt="Humidity Image" layout="fill" objectFit="cover" />
       <div className="absolute inset-0 flex flex-col justify-center items-center mt-[4.7rem]">
         <h1 className='text-white font-extrabold text-sm'>Humidity</h1>
-        <p className='text-white text-xs'>50%</p>
+        <p className='text-white text-xs'>{humidityPersentage}%</p>
       </div>
     </div>
   )
