e0bbbb2f7c9ad30e1bfabd42af273c02d8e30f04
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-06-07 21:58:33 +0700

    style: change box size

--- app/Components/UvIndex.tsx
+++ app/Components/UvIndex.tsx
@@ -4,7 +4,7 @@ import uvImage from "../../public/uv-image.png"
 
 export default function UvIndex() {
   return (
-    <div className="relative w-[170px] h-[170px]">
+    <div className="relative w-[190px] h-[190px]">
       <Image src={uvImage} alt="UV Image" layout="fill" objectFit="cover" />
       <div className="absolute inset-0 flex flex-col justify-center items-center mt-[4.7rem]">
         <h1 className='text-white font-extrabold text-sm'>Uv Index</h1>
