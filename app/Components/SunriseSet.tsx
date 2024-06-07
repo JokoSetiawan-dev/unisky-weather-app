ba4cd655a1e595a18aceec33ce77c96ce9ca0cba
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-06-07 21:58:10 +0700

    feat: get time data from api

--- app/Components/SunriseSet.tsx
+++ app/Components/SunriseSet.tsx
@@ -2,19 +2,34 @@ import React from 'react'
 import Image from 'next/image'
 import sunriseIcon from "../../public/sunrise-icon.png"
 import sunsetIcon from "../../public/sunset-icon.png"
+import { useGlobalContext } from '../context/globalContext'
+import { getTimeFromTimestamp } from '../utils/misc'
+
 
 export default function SunriseSet() {
+
+  const {forecast} = useGlobalContext()
+
+  const {sys} = forecast
+
+  console.log(sys?.sunrise)
+
+  const sunriseTime = getTimeFromTimestamp(sys?.sunrise)
+  const sunsetTime = getTimeFromTimestamp(sys?.sunset)
+
   return (
-    <div className='flex w-[185.2px] h-[110px] justify-around bg-white rounded-3xl shadow-lg'>
-      <div className='flex flex-col items-center justify-center'>
-        <Image src={sunriseIcon} alt='sunrise icon'/>
-        <h1 className='font-bold text-sm mt-2'>Sunrise</h1>
-        <p className='text-sm'>06:00</p>
-      </div>
-      <div className='flex flex-col items-center justify-center'>
-        <Image src={sunsetIcon} alt='sunrise icon'/>
-        <h1 className='font-bold text-sm mt-2'>Sunset</h1>
-        <p className='text-sm'>18:00</p>
+    <div className='flex w-[230px] h-[110px] justify-around bg-white rounded-3xl shadow-lg'>
+      <div className='flex flex-row items-center gap-10'>
+        <div className='flex flex-col items-center justify-center'>
+          <Image src={sunriseIcon} alt='sunrise icon'/>
+          <h1 className='font-bold text-sm mt-2'>Sunrise</h1>
+          <p className='text-sm'>{sunriseTime}</p>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <Image src={sunsetIcon} alt='sunrise icon'/>
+          <h1 className='font-bold text-sm mt-2'>Sunset</h1>
+          <p className='text-sm'>{sunsetTime}</p>
+        </div>
       </div>
     </div>
   )
