b4383fec46e467c26848093eecb0bb02e0f5cfbf
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-06-07 21:59:10 +0700

    feat: add function to get time from timestamp

--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,9 +1,20 @@
-export const kelvinToCelsius = (kelvin:number) => {
-    return Math.round(kelvin - 273.15)
-}
+export const kelvinToCelsius = (kelvin: number) => {
+  return Math.round(kelvin - 273.15);
+};
 
 export const toProperCase = (str: string) => {
-    return str.replace(/\w\S*/g, (word) =>
-        word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
-    );
+  return str.replace(
+    /\w\S*/g,
+    (word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+  );
+};
+
+export const getTimeFromTimestamp = (timestamp: number): string => {
+
+  const date = new Date(timestamp * 1000);
+
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${hours}:${minutes}`;
 };
