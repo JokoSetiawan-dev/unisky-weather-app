deaf9fe73f98718ebe6a27d9c42cbdb542b5e1f7
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-06-24 18:44:08 +0700

    style: add dynamically background color based on day and night values

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,23 +1,37 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import uniskyLogo from "../../public/unisky-logo.svg"
-import searchIcon from "../../public/search.svg"
 import Image from 'next/image'
-import locationIcon from "../../public/location-icon.svg"
 import Search from './Ui/Search'
 
 function Navbar() {
+  const [isDayTime, setIsDayTime] = useState(true)
+
+  useEffect(() => {
+    const determineDayOrNight = () => {
+      const hours = new Date().getHours()
+      const dayTime = hours >= 6 && hours < 18
+      setIsDayTime(dayTime)
+    }
+
+    determineDayOrNight()
+
+    const intervalId = setInterval(determineDayOrNight, 60000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
+  const backgroundColor = isDayTime ? '#81ACDA' : '#322660'
+
   return (
-    <>
-      <div className='z-10 w-full top-0'>
-      <div className='flex items-center top-0 bg-[#81ACDA] w-full h-[72px]'>
+    <div className='z-10 w-full top-0' style={{ backgroundColor }}>
+      <div className='flex items-center top-0 w-full h-[72px]'>
         <div className='flex flex-row items-center justify-between w-full px-5'>
           <Image className='w-[82px] h-auto' src={uniskyLogo} alt='unisky logo'/> 
           <Search/>
         </div>
       </div>
-      </div>
-    </>
+    </div>
   )
 }
 
