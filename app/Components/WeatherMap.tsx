50edd5cbd930b892cb20d54a8ab1ba218442197b
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-08-11 01:13:29 +0700

    feat dynamic declaration

--- app/Components/WeatherMap.tsx
+++ app/Components/WeatherMap.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
