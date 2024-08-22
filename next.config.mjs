e89c77da8b4629a46674f7e9edc224706019b853
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-08-22 23:22:37 +0700

    feat: use csp for security

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/(.*)", // Applies the CSP to all routes
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: `
+                default-src 'self';
+                script-src 'self';
+                connect-src 'self' http://api.openweathermap.org https://api.openweathermap.org https://api.open-meteo.com;
+                img-src 'self' data:;
+                style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+                font-src 'self' https://fonts.gstatic.com;
+              `.replace(/\s{2,}/g, ' ').trim(), // Clean up extra spaces
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
